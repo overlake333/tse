10b6b2cbfac75275e42ec9d4fc5f3e2277633448
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2019-10-15 19:41:58 -0400

    we did some serious stuff like woah, insane

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -10,8 +10,60 @@
  */
 
 #include <stdio.h>
+#include "webpage.h"
+#include "queue.h"
+#include "hash.h"
+#include <stdbool.h>
+
+void qprint(queue_t *q);
+
 
 int main(void){
-  printf("Hello");
-  return 0;
+  
+  webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+
+  if(webpage_fetch(page)){
+    
+    int pos = 0;
+    char *result;
+    queue_t *webpages = qopen();
+    webpage_t *url;
+    
+    while((pos = webpage_getNextURL(page, pos, &result)) > 0){
+      if(IsInternalURL(result)){
+	url = webpage_new(result, 1, NULL);
+	qput(webpages, (void *)url);
+	printf("%s\n", webpage_getURL(url));
+      }
+      free(result);
+    }
+
+    webpage_delete(url); // valgrind error ye
+
+    
+
+    qclose(webpages);
+    webpage_delete(page);
+
+  } else {
+    exit(EXIT_FAILURE);
+  }
+ 
+  
+  exit(EXIT_SUCCESS);
 }
+
+
+void qprint(queue_t *q){
+
+  char *page;
+  
+  while((page = webpage_getURL((webpage_t *)qget(q))) != NULL){
+    printf("%s\n", page);
+   
+  }
+  free(page);
+  
+
+}
+

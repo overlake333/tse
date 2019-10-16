a5836079091faff082da0c8159643f2201d2ae36
James Fleming <Jamesdfleming1@gmail.com>
2019-10-16 11:53:51 -0400

    finished step 4git add .!

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -10,60 +10,64 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "webpage.h"
 #include "queue.h"
 #include "hash.h"
 #include <stdbool.h>
 
-void qprint(queue_t *q);
-
+static bool compareByURL(void* elementp, const void* searchkeyp){
+	// this function needs to see if the url searchkeyp is the same as the url from the element
+	if (strcmp(webpage_getURL((webpage_t *)elementp), (const char *)searchkeyp) == 0){
+		return true;
+	}
+	return false;
+}
 
 int main(void){
-  
+  // Create a new webpage
   webpage_t *page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
-
-  if(webpage_fetch(page)){
-    
+	// Grab the webpage's html
+	hashtable_t *visited = hopen(20);
+	
+	if(webpage_fetch(page)){
+		// add this page to the list of visited webpages
+		const char *key = (const char *)(webpage_getURL(page));
+		hput(visited, (void *)page, key, sizeof(key));
     int pos = 0;
     char *result;
     queue_t *webpages = qopen();
     webpage_t *url;
-    
+    //Grab the next url from the page starting at pos 0
     while((pos = webpage_getNextURL(page, pos, &result)) > 0){
       if(IsInternalURL(result)){
-	url = webpage_new(result, 1, NULL);
-	qput(webpages, (void *)url);
-	printf("%s\n", webpage_getURL(url));
-      }
-      free(result);
-    }
+				if (hsearch(visited, compareByURL, (const char *)result, sizeof((const char *)result)) == NULL){
+					url = webpage_new(result, 1, NULL);
+					hput(visited, url, (const char *)result, sizeof((const char *)result));
+					qput(webpages, (void *)url);
+				}
+			}
+			free(result);
+		}
+		
 
-    webpage_delete(url); // valgrind error ye
-
+		// Now that all the webpages are in the queue lets run through it printing the webpages and freeing them as we go.
+    webpage_t *w = (webpage_t *)qget(webpages);
+		while (w != NULL){
+			printf("%s\n", webpage_getURL(w));
+			webpage_delete(w);
+			w =(webpage_t *)qget(webpages);
+		}
     
-
     qclose(webpages);
     webpage_delete(page);
-
+		hclose(visited);
   } else {
     exit(EXIT_FAILURE);
   }
  
-  
+	
   exit(EXIT_SUCCESS);
 }
 
-
-void qprint(queue_t *q){
-
-  char *page;
-  
-  while((page = webpage_getURL((webpage_t *)qget(q))) != NULL){
-    printf("%s\n", page);
-   
-  }
-  free(page);
-  
-
-}
 

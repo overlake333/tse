f1650851ba5c2837b669cc1944bba71ba0ffef58
James Fleming <Jamesdfleming1@gmail.com>
2019-10-24 16:24:54 -0400

    finished testPageio.c

--- test/testPageio.c
+++ test/testPageio.c
@@ -10,9 +10,18 @@
  */
 #include <pageio.h>
 #include <webpage.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void){
- 	webpage_t *mypage = pageload(1, "pages");
-	pagesave(mypage, 11, "newpages");
-	//webpage_t *loadback = pageload(6, "newpages");
+ 	webpage_t *mypage = pageload(2, "pages");
+	pagesave(mypage, 12, "newpages");
+	webpage_t *loadback = pageload(12, "newpages");
+	if (strcmp((webpage_getHTML(mypage)), (webpage_getHTML(loadback))) != 0){
+		printf("Test failed, html somehow changed between saves and loads\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("Test Succeeded!\n");
+	exit(EXIT_SUCCESS);
 }

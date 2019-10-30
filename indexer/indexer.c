e947b1c4b9a9d2fb41525375b6415468fd7fc822
James Fleming <Jamesdfleming1@gmail.com>
2019-10-30 17:05:06 -0400

    indexer works

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -21,15 +21,112 @@
 #include <indexio.h>
 
 /* INTERNAL FUNCTION PROTOTYPES */
+void NormalizeWord(char *word);
 
-
-
-
-int main(int argc, char *argv[]) {
+typedef struct hashIndex{
+	char *word;
 	
+	// frequency is outdated
+	//int frequency;
+	// queue pointer of webpages that have this word
+	queue_t *pages;
+	
+	
+} hashIndex_t;
+
+
+// MAKE A STRUCT THAT HOLDS A WEBPAGE and frequency of a word asssociated with that webpage
+
+typedef struct wordPage{
+	int id; // webpage id
+	int frequency; // word frequency
+} wordPage_t;
+
+// make a queue of these types 
+
+wordPage_t *makeWordPage(int id) {
+	wordPage_t *page;
+	if (!(page = (wordPage_t *)malloc(sizeof(wordPage_t)))){
+		return NULL;
+	}
+	
+	page->id = id;
+	page->frequency = 1;
+	
+	return page;
+}
+
+hashIndex_t *makeIndex(char *word, int id){
+	hashIndex_t *Hi;
+	wordPage_t *page;
+	if (!(Hi = (hashIndex_t *)malloc(sizeof(hashIndex_t)))){
+		return NULL;
+	}
+	Hi->pages = qopen();
+	page = makeWordPage(id);
+	qput(Hi->pages, (void *)page);
+	
+	Hi->word = word;
+	return Hi;
+}
+
+void deleteWordPage(void *wordPage){
+	free(wordPage);
+}
+
+void deleteIndex(void *hashIndex){
+	hashIndex_t *hi = (hashIndex_t *)hashIndex;
+	queue_t *myQueue = hi->pages;
+	qapply(myQueue, deleteWordPage);
+	qclose(myQueue);
+	free(hi->word);
+	free(hashIndex);
+}
+
+
+
+
+
+static bool searchfn(void *elementp, const void* searchkeyp){
+	hashIndex_t *Hi = (hashIndex_t *)elementp;
+	if (strcmp(Hi->word, searchkeyp) == 0){
+		return true;
+	}
+	return false;
+}
+
+static bool qsearchfn(void *elementp, const void *searchkeyp) {
+	wordPage_t *myWordPage = (wordPage_t *)elementp;
+	if(strcmp((const char *)(&(myWordPage->id)), searchkeyp) == 0) {
+		return true;
+	}
+	return false;
+}
+/*
+static int wordCount = 0;
+
+static void addToCount2(void *ep) {
+	wordPage_t *wp = (wordPage_t *)ep;
+	wordCount = wordCount + (wp->frequency);
+}
+
+static void addToCount(void *ep){
+	hashIndex_t *hi = (hashIndex_t *)ep;
+	qapply(hi->pages, addToCount2); 
+	//	wordCount = wordCount + (hi->frequency);
+}
+
+static int sumwords(hashtable_t *ht){
+	happly(ht, addToCount);
+	return wordCount;
+	
+}
+*/
+
+// Main
+int main(int argc, char *argv[]) {
+
 	char *usage = "usage: indexer <pagedir> <indexnm>";
-	// Loads a webpage with pageio module
-	// int toId = atoi(argv[1]);
 	if (argc != 3) {
 		printf("%s\n", usage);
 		exit(EXIT_FAILURE);
@@ -37,43 +134,28 @@ int main(int argc, char *argv[]) {
 	char *pagedir = argv[1];
 	char *indexnm = argv[2];
 	hashtable_t *index = hopen(50);
-
 	
 	
-	//NEEDS TO CHECK IF DIR EXISTS
-	//	indexload(index, pagedir);
-
-
-	indexsave(index, indexnm);
-<<<<<<< HEAD
-	/*
-	for (int id = 1; id <= toId; id++) {
-		webpage_t *page = pageload(id, "pages");
+	int id =1;
+	webpage_t *page;
+	
+	while((page = pageload(id, pagedir)) != NULL) {
 		int pos = 0;
 		char *word;
-		char out[50];
-		sprintf(out, "out%d.txt", id);
-		FILE *output = fopen(out, "w"); // output file to write html to
-		
-		// Uses webpage_getNextWord to print all words in HTML. SKIPS TAGS! 		
-		
 		
 		while ((pos = webpage_getNextWord(page, pos, &word)) > 0) {
-			//printf("%s\n", word);
 			NormalizeWord(word);
 			if (strlen(word) >= 3){
-				fprintf(output, "%s\n", word); // prints the words of the html to a file
-				// if word is not already in hash table
-				hashIndex_t *hw = (hashIndex_t *)(hsearch(index, searchfn, (const char *)word, sizeof(word)));
+				hashIndex_t *hw = (hashIndex_t *)(hsearch(index, searchfn, (const char *)word, strlen(word)));
 				if (hw == NULL){
 					// create a new hashIndex_t
 					hashIndex_t *ht = makeIndex(word, id);
 					// add it to hashtable
-					hput(index, ht, (const char *)word, sizeof(word));
+					hput(index, ht, (const char *)word, strlen(word));
 					
 				} else {
 					// update the frequency of the hashindex_t by 1
-					//hw->frequency = hw->frequency +1;
+					free(word);
 					wordPage_t *wp = (wordPage_t *)(qsearch(hw->pages, qsearchfn, (const char *)&id));
 					if (wp == NULL) {
 						// create new wordPage - has a id and frequency!
@@ -83,24 +165,37 @@ int main(int argc, char *argv[]) {
 					} else {
 						// Update a wordPage's frequency by 1
 						wp->frequency = wp->frequency +1;
-					}
-					
+					}					
 				}
-				
-				
+			} else {
+				free(word);
 			}
-			//free(word);
 		}
-	}
-	*/
-	//	printf("%d\n", sumwords(index));
+		webpage_delete(page);
+		id++;
+	 }
+	indexsave(index, indexnm);
 	happly(index, deleteIndex);
-=======
- 
->>>>>>> ddea77a245b96dcfd203abecc7784bbb0c5ad331
 	hclose(index);
 	exit(EXIT_SUCCESS);
 }
 
 
+/* INTERNAL FUNCTIONS */
+
+
+/* NormalizeWord - 
+ * Converts a word to lowercase
+ *
+ * Inputs: word - a string
+ */ 
+void NormalizeWord(char *word) {
+
+	if (word != NULL) {
+		for (char *c = word; *c != '\0'; c++) {
+			*c = (char)tolower(*c);
+		}
+	}
+
+}
 

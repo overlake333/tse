a7f2795cb616cabea058abf3b9599939158c45d9
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2019-11-01 13:30:33 -0400

    hear about the guy who's left side was cut off? hes alright now

--- querier/query.c
+++ querier/query.c
@@ -12,6 +12,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <indexio.h>
+#include <hash.h>
+#include <webpage.h>
+
+typedef struct queryPage{
+	int rank;
+	int id;
+	webpage_t *page;
+} queryPage_t;
+
+typedef struct wordPage{
+  int id; // webpage id
+  int frequency; // word frequency
+} wordPage_t;
+
+typedef struct hashIndex{
+  char *word;
+  // frequency is outdated
+  //int frequency;
+  // queue pointer of webpages that have this word
+  queue_t *pages;
+} hashIndex_t;
+
+
+void deleteWordPage(void *wordPage){
+	free(wordPage);
+}
+
+void deleteIndex(void *hashIndex){
+	hashIndex_t *hi = (hashIndex_t *)hashIndex;
+	queue_t *myQueue = hi->pages;
+	qapply(myQueue, deleteWordPage);
+	qclose(myQueue);
+	free(hi->word);
+	free(hashIndex);
+}
+
+
+static bool searchfn(void *elementp, const void* searchkeyp){
+  hashIndex_t *Hi = (hashIndex_t *)elementp;
+  if (strcmp(Hi->word, searchkeyp) == 0){
+    return true;
+  }
+  return false;
+}
+
+
+static bool qsearchfn(void *elementp, const void *searchkeyp) {
+  wordPage_t *myWordPage = (wordPage_t *)elementp;
+  if(strcmp((const char *)(&(myWordPage->id)), searchkeyp) == 0) {
+    return true;
+  }
+  return false;
+}
+
+
+
+
 
 static int readline(char **string){
   *string = (char *)malloc(100);
@@ -38,7 +96,7 @@ static int readline(char **string){
 	}
 	*p = '\0';
 	// successful creation of string return 1
-	//	fflush(stdin);
+
 	return 1;
 }
 
@@ -49,41 +107,56 @@ int main(void){
 	char *line;
 	char array[50][100];
 	int value;
+	hashtable_t *index = indexload("../indexer/querytest.txt");
+	
+	
 	while ((value = readline(&line)) != -1){
 		int i = 0;
 		if (value == 1){
-			//printf("%s\n", line);
 			char *token = strtok(line, " ");
-			
 			while (token != NULL) {
 				strcpy(array[i], token);
 				token = strtok(NULL, " ");
 				i++;
 		 	}
 			int j = 0;
+			int min;
 			while (j < i) {
-				printf("%s ", array[j]);
+				if (strcmp(array[j], "and") != 0 && strcmp(array[j], "or") != 0) {
+					hashIndex_t *hi = (hashIndex_t *)(hsearch(index, searchfn, (const char *)array[j], strlen(array[j])));			
+					if (hi != NULL) {
+						int id = 1;
+						wordPage_t *mywp = (wordPage_t *)(qsearch(hi->pages, qsearchfn, (const char *)&id));
+						if (mywp != NULL) {
+							int frequency = mywp->frequency;
+							if (j == 0) {
+								min = frequency;
+							} else if (frequency < min) {
+								min = frequency;
+							}
+							printf("%s:%d ", array[j], frequency);
+						} else {
+							// In case query has a word not in the page we are looking at
+							min = 0;
+						}
+					} else {
+						// In case a word in the query is not in the index
+						min = 0;
+					}
+				}
 				j++;
 			}
-			printf("\n");
-			
-
+			printf("-- %d\n", min); 
 		}
 		if (value == 2){
-			
 			printf("[invalid query]\n");
 		}
-		fflush(stdin);
 		value = 0;
 		printf("> ");
 		free(line);
 	}
-
-	
-
-
-	
-	
-	free(line);	
-	return 0;
+	free(line);		
+	happly(index, deleteIndex);
+	hclose(index);	
+ 	return 0;
 }
